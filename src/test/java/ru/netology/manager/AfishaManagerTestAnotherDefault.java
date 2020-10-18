029b0d84f89292dab346139fb32fa1b7a925de34
Kirill <kiruxastar@gmail.com>
2020-10-18 12:13:51 +0300

    New files added

--- src/test/java/ru/netology/manager/AfishaManagerTestAnotherDefault.java
+++ src/test/java/ru/netology/manager/AfishaManagerTestAnotherDefault.java
@@ -2,12 +2,25 @@ package ru.netology.manager;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import ru.netology.domain.MovieItem;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
 
-class MovieManagerTestAnotherDefault {
-    private MovieManager manager = new MovieManager(5);
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.netology.domain.MovieItem;
+import ru.netology.repository.AfishaRepository;
+
+@ExtendWith(MockitoExtension.class)
+
+public class AfishaManagerTestAnotherDefault {
+
+    @Mock
+    private AfishaRepository repository;
+    @InjectMocks
+    private AfishaManager manager = new AfishaManager(5, repository);
     MovieItem first = new MovieItem(1, 10, "Tears in the night", "drama", 20200820);
     MovieItem second = new MovieItem(2, 20, "Snickering killer", "comedy", 20200828);
     MovieItem third = new MovieItem(3, 30, "Notsnickering keller", "horror", 20200828);
@@ -15,18 +28,22 @@ class MovieManagerTestAnotherDefault {
     MovieItem fifth = new MovieItem(5, 50, "Space chase", "fantastic", 20200910);
     MovieItem sixth = new MovieItem(6, 60, "Axe and wax", "horror", 20200910);
 
-
     @BeforeEach
     public void setUp() {
         manager.add(first);
         manager.add(second);
         manager.add(third);
         manager.add(forth);
+//        manager.add(fifth);
+//        manager.add(sixth);
     }
 
     @Test
     public void shouldGetLessThanDefault() {
-        manager.setMoviesNumberAtDefault(manager.getMoviesNumberAtDefault());
+
+        MovieItem[] returned = new MovieItem[]{first, second, third, forth};
+        doReturn(returned).when(repository).findAll();
+
         MovieItem[] actual = manager.getLastTenOrLess();
         MovieItem[] expected = new MovieItem[]{forth, third, second, first};
         assertArrayEquals(expected, actual);
@@ -35,8 +52,11 @@ class MovieManagerTestAnotherDefault {
     @Test
     public void shouldGetDefault() {
         manager.add(fifth);
+        MovieItem[] returned = new MovieItem[]{first, second, third, forth, fifth};
+        doReturn(returned).when(repository).findAll();
+
         MovieItem[] actual = manager.getLastTenOrLess();
-        MovieItem[] expected = new MovieItem[]{ fifth, forth, third, second, first};
+        MovieItem[] expected = new MovieItem[]{fifth, forth, third, second, first};
         assertArrayEquals(expected, actual);
     }
 
@@ -44,9 +64,12 @@ class MovieManagerTestAnotherDefault {
     public void shouldGetMoreThanDefault() {
         manager.add(fifth);
         manager.add(sixth);
-        manager.setMoviesNumberAtDefault(manager.getMoviesNumberAtDefault());
+        MovieItem[] returned = new MovieItem[]{first, second, third, forth, fifth, sixth};
+        doReturn(returned).when(repository).findAll();
+
         MovieItem[] actual = manager.getLastTenOrLess();
-        MovieItem[] expected = new MovieItem[]{sixth, fifth, forth, third, second };
+        MovieItem[] expected = new MovieItem[]{sixth, fifth, forth, third, second};
         assertArrayEquals(expected, actual);
     }
+
 }
